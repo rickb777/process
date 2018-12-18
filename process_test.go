99f13e348165dbd0632694f0f86210e76c578e9b
Rick Beton <rick.beton@infosum.com>
2018-12-18 15:17:29 +0000

    tests

--- process_test.go
+++ process_test.go
@@ -5,36 +5,35 @@ import (
 )
 
 func TestProcessGroup(t *testing.T) {
-	n1 := false
-	n2 := false
-	n3 := false
-	n4 := false
-	n5 := false
-
 	pg := NewGroup()
 
-	pg.Go(func() { n1 = true })
-	pg.Go(func() { n2 = true })
-	pg.Go(func() { n3 = true })
-	pg.Go(func() { n4 = true })
-	pg.Go(func() { n5 = true })
+	// expect to be able to make more than one pass
+	for p := 0; p < 1000; p++ {
+		n1, n2, n3, n4, n5 := false, false, false, false, false
 
-	pg.Join()
+		pg.Go(func() { n1 = true })
+		pg.Go(func() { n2 = true })
+		pg.Go(func() { n3 = true })
+		pg.Go(func() { n4 = true })
+		pg.Go(func() { n5 = true })
 
-	if !n1 {
-		t.Errorf("no n1")
-	}
-	if !n2 {
-		t.Errorf("no n2")
-	}
-	if !n3 {
-		t.Errorf("no n3")
-	}
-	if !n4 {
-		t.Errorf("no n5")
-	}
-	if !n5 {
-		t.Errorf("no n5")
+		pg.Join()
+
+		if !n1 {
+			t.Errorf("no n1")
+		}
+		if !n2 {
+			t.Errorf("no n2")
+		}
+		if !n3 {
+			t.Errorf("no n3")
+		}
+		if !n4 {
+			t.Errorf("no n5")
+		}
+		if !n5 {
+			t.Errorf("no n5")
+		}
 	}
 }
 
@@ -42,36 +41,44 @@ func TestProcessGroupN0(t *testing.T) {
 	c := make(chan int)
 	pg := NewGroup()
 
-	pg.GoN(3, func() { c <- 1 })
+	// expect to be able to make more than one pass
+	for p := 0; p < 1000; p++ {
+		pg.GoN(5, func() { c <- 1 })
 
-	var sum int
-	sum += <-c
-	sum += <-c
-	sum += <-c
+		var sum int
+		sum += <-c
+		sum += <-c
+		sum += <-c
+		sum += <-c
+		sum += <-c
 
-	if sum != 3 {
-		t.Errorf("Got %d", sum)
+		if sum != 5 {
+			t.Errorf("Got %d", sum)
+		}
+
+		pg.Join() // no deadlock expected
 	}
-
-	pg.Join() // no deadlock expected
 }
 
 func TestProcessGroupN1(t *testing.T) {
 	c := make(chan int)
 	pg := NewGroup()
 
-	pg.GoN1(5, func(i int) { c <- i })
+	// expect to be able to make more than one pass
+	for p := 0; p < 1000; p++ {
+		pg.GoN1(5, func(i int) { c <- i })
 
-	var sum int
-	sum += <-c
-	sum += <-c
-	sum += <-c
-	sum += <-c
-	sum += <-c
+		var sum int
+		sum += <-c
+		sum += <-c
+		sum += <-c
+		sum += <-c
+		sum += <-c
 
-	if sum != 15 {
-		t.Errorf("Got %d", sum)
+		if sum != 15 {
+			t.Errorf("Got %d", sum)
+		}
+
+		pg.Join() // no deadlock expected
 	}
-
-	pg.Join() // no deadlock expected
 }
