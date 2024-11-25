eb91e3d1dab5e3f71c6b52b07337331bc1fcfc60
Rick <1358735+rickb777@users.noreply.github.com>
2024-11-25 10:03:12 +0000

    Now uses Deque v1.0.0

--- channel.go
+++ channel.go
@@ -7,14 +7,15 @@ import "github.com/gammazero/deque"
 // this will close the output end.
 //
 // This long channel is useful as a work queue because items can be added arbitrarily
-// to it without deadlock (although there is no guarantee that memory won't run out),
-// even if the channel connectivity graph contains loops (i.e. contains the risk of
-// deadlock). Each process inserting into the queue will never be blocked.
+// to it without deadlock, even if the channel connectivity graph contains loops
+// (i.e. contains the risk of deadlock). Each process inserting into the queue will
+// never be blocked. However, there is no guarantee that memory won't run out, so
+// users must consider avoidance of deadlock in their design.
 //
 // The channel's buffer is a ring buffer in memory, which is of course not persistent
-// across restarts. An initial size can be provided; this can be zero for the default
-// size.
-func WorkQueue[T any](initialSize uint) (chan<- T, <-chan T) {
+// across restarts. A minimum capacity can be provided; this can be absent or zero for
+// the default size.
+func WorkQueue[T any](minimumCapacity ...uint) (chan<- T, <-chan T) {
 	in := make(chan T, 1)
 	out := make(chan T)
 
@@ -22,7 +23,10 @@ func WorkQueue[T any](initialSize uint) (chan<- T, <-chan T) {
 		canIn := in
 		var canOut chan T
 		var last T
-		buffer := deque.New[T](int(initialSize), int(initialSize))
+		var buffer deque.Deque[T]
+		if len(minimumCapacity) > 0 {
+			buffer.SetBaseCap(int(minimumCapacity[0]))
+		}
 
 		for canIn != nil || canOut != nil {
 			select {
--- go.mod
+++ go.mod
@@ -2,10 +2,4 @@ module github.com/rickb777/process/v2
 
 go 1.22
 
-require github.com/gammazero/deque v0.2.1
-
-require (
-	github.com/mattn/goveralls v0.0.12 // indirect
-	golang.org/x/mod v0.10.0 // indirect
-	golang.org/x/tools v0.8.0 // indirect
-)
+require github.com/gammazero/deque v1.0.0
