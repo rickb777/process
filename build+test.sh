91a2376f58f775a4887a983752dc267a2a06c859
Rick <1358735+rickb777@users.noreply.github.com>
2025-04-30 23:23:32 +0100

    now using Mage build engine

--- build+test.sh
+++ build+test.sh
@@ -1,14 +1,3 @@
-#!/bin/bash -e
-PATH=$HOME/gopath/bin:$GOPATH/bin:$PATH
-
-if ! type -p goveralls; then
-  echo go get github.com/mattn/goveralls
-  go get github.com/mattn/goveralls
-fi
-
-go test -v -race .
-go test -v -covermode=count -coverprofile=date.out .
-go tool cover -func=date.out
-[ -z "$COVERALLS_TOKEN" ] || goveralls -coverprofile=date.out -service=travis-ci -repotoken $COVERALLS_TOKEN
-
-go vet ./...
+#!/bin/bash -ex
+go install tool
+mage
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/rickb777/process/v2
 go 1.22
 
 require github.com/gammazero/deque v1.0.0
+
+require github.com/magefile/mage v1.15.0
